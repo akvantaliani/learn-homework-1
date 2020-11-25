5ec30da4c1ffacf5139adcd019b86e35c6ad6a5b
Alexander Kvantaliani <akvantal@yandex.com>
2020-11-25 23:27:16 +0300

    Completed 6th task on the topic: Exception handling

--- 6_exception1.py
+++ 6_exception1.py
@@ -1,20 +1,14 @@
-"""
-
-Домашнее задание №1
-
-Исключения: KeyboardInterrupt
-
-* Перепишите функцию hello_user() из задания while1, чтобы она 
-  перехватывала KeyboardInterrupt, писала пользователю "Пока!" 
-  и завершала работу при помощи оператора break
-    
-"""
+# Перепишите функцию hello_user() из задания про while, чтобы она перехватывала KeyboardInterrupt,
+# писала пользователю "Пока!" и завершала работу при помощи оператора break
 
 def hello_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
+    user_input = ""
+    while user_input != "Хорошо":
+        try:
+            user_input = input("Как дела?\n")
+        except KeyboardInterrupt:
+            print(" Пока!")
+            break
     
 if __name__ == "__main__":
     hello_user()
