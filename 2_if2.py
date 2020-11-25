43705918d8fa41077ad99e4608e1948ff25c9898
Alexander Kvantaliani <akvantal@yandex.com>
2020-11-25 21:17:12 +0300

    Completed the second task on the topic: if statement.

--- 2_if2.py
+++ 2_if2.py
@@ -1,26 +1,19 @@
-"""
+def main(first_str, second_str):
+    if type(first_str) != str or type(second_str) != str:
+        return 0
+    elif len(first_str) == len(second_str):
+        return 1
+    elif len(first_str) > len(second_str):
+        return 2
+    elif second_str == 'learn':
+        return 3
 
-Домашнее задание №1
 
-Условный оператор: Сравнение строк
-
-* Написать функцию, которая принимает на вход две строки
-* Проверить, является ли то, что передано функции, строками. 
-  Если нет - вернуть 0
-* Если строки одинаковые, вернуть 1
-* Если строки разные и первая длиннее, вернуть 2
-* Если строки разные и вторая строка 'learn', возвращает 3
-* Вызвать функцию несколько раз, передавая ей разные праметры 
-  и выводя на экран результаты
-
-"""
-
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-    
 if __name__ == "__main__":
-    main()
+    print(main({'name':'learn'}, 123))            #0
+    print(main('Learn', 'learn'))                 #1
+    print(main('python', 'Learn'))                #2
+    print(main('do', 'learn'))                    #3
+    print(main('python', 'learn'))                #2
+    print(main(['learn','python','ru'], 'learn')) #0
+    print(main('Learn', 'python'))                #None
