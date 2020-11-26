7af5d521bc901a4fa2d2ed3a79c53cdb47c329ff
Alexander Kvantaliani <akvantal@yandex.com>
2020-11-26 21:39:04 +0300

    Completed last task on the topic: Modules

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -1,20 +1,15 @@
-"""
-Домашнее задание №1
-
-Использование библиотек: ephem
-
-* Установите модуль ephem
-* Добавьте в бота команду /planet, которая будет принимать на вход
-  название планеты на английском, например /planet Mars
-* В функции-обработчике команды из update.message.text получите
-  название планеты (подсказка: используйте .split())
-* При помощи условного оператора if и ephem.constellation научите
-  бота отвечать, в каком созвездии сегодня находится планета.
-
-"""
+# Установите модуль ephem
+# Добавьте в бота команду /planet, которая будет принимать на вход название планеты
+# на английском, например /planet Mars
+# В функции-обработчике команды из update.message.text получите
+# название планеты (подсказка: используйте .split())
+# При помощи условного оператора if и ephem.constellation научите бота отвечать,
+# в каком созвездии сегодня находится планета.
 import logging
-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import settings
+import ephem
+from datetime import date
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -22,10 +17,10 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 
 
 PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'proxy_url': settings.PROXY_URL,
     'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
+        'username': settings.PROXY_USERNAME,
+        'password': settings.PROXY_PASSWORD
     }
 }
 
@@ -39,14 +34,38 @@ def greet_user(update, context):
 def talk_to_me(update, context):
     user_text = update.message.text
     print(user_text)
-    update.message.reply_text(text)
+    update.message.reply_text(user_text)
+
+
+def find_planet(update, context):
+    user_text = update.message.text.split()
+    if len(user_text) > 1:
+        today_str = date.today().strftime("%Y/%m/%d")
+        planet_input = user_text[1].capitalize()
+        try:
+            planet_class = getattr(ephem, planet_input)
+            planet_today = planet_class(today_str)
+            constellation_tuple = ephem.constellation(planet_today)
+            constellation = constellation_tuple[1]
+            constellation_to_reply = f'{planet_input} находится в созвездии "{constellation}".'
+            print(constellation_to_reply)
+            update.message.reply_text(constellation_to_reply)
+        except AttributeError:
+            wrong_planet = f'Я не знаю небесное тело с названием "{planet_input}".'
+            print(wrong_planet)
+            update.message.reply_text(wrong_planet)
+    else:
+        empty_planet_reply = 'Вы не назвали небесное тело.'
+        print(empty_planet_reply)
+        update.message.reply_text(empty_planet_reply)
 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater(settings.API_KEY, request_kwargs=PROXY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", find_planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
