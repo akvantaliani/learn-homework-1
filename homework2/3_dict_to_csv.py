350091d8eb09bafd4ae246c05386a166035f0a5e
Alexander Kvantaliani <akvantal@yandex.com>
2020-12-03 01:41:03 +0300

    Completed 3rd task on the topic: dict to csv

--- homework2/3_dict_to_csv.py
+++ homework2/3_dict_to_csv.py
@@ -10,15 +10,39 @@
 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
+import csv
 
 
-def main():
+def export_csv(people_list):
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open('export.csv', 'w', encoding='utf-8', newline='') as csv_file:
+        fields = ['name', 'age', 'job']
+        writer = csv.DictWriter(f=csv_file, fieldnames=fields, delimiter=';')
+        writer.writeheader()
+        gen = (info_dict for info_dict in people_list)
+        for human in gen:
+            if human:
+                try:
+                    info_list = {}
+                    info_list['name'] = human.get('name', None)
+                    info_list['age'] = human.get('age', None)
+                    info_list['job'] = human.get('job', None)
+                    writer.writerow(info_list)
+                except AttributeError:
+                    print(f'"{human}" не является словарём')
+            else:
+                continue
 
 
 if __name__ == "__main__":
-    main()
+    workers_list = [
+        {'name': 'Joe Malto', 'age': 30, 'job': 'engineer'},
+        {'name': 'Terry Dallas', 'age': 33, 'job': 'programmer'},
+        {'name': 'Kate Brown', 'age': 29, 'job': 'actress'},
+        {'name': 'Anna Marana', 'age': 22, 'job': 'vet'},
+        {'name': 'Bill Kogan', 'age': 66, 'job': 'postman'},
+    ]
+    export_csv(workers_list)
